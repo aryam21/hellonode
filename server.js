77028f681c0c0ec52b42df066dbdcc0b3ed2e9d7
aryam21 <98018371+aryam21@users.noreply.github.com>
2023-01-16 15:20:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+
 var ip = require("ip");
-
-const { collectDefaultMetrics, register } = require('prom-client');
-
-collectDefaultMetrics({
-	timeout: 10000,
-	gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5], // These are the default buckets.
-});
-
-console.log(register.metrics());
-
 var cors = require("cors");
+
+// Import the dd-trace package
+const tracer = require('dd-trace').init();
+
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(cors());
 app.get('/message', (req, res) => {
+  // Start a new trace
+  const span = tracer.startSpan('/message');
+  // Your logic here
+  span.finish();
   return res.send(process.env.PAGE_TITLE)
 })
 app.get('/color', (req, res) => {
+  // Start a new trace
+  const span = tracer.startSpan('/color');
+  // Your logic here
   if (process.env.PAGE_COLOR==="Blue" || process.env.PAGE_COLOR==="blue")
     return res.send("#053354")
   if (process.env.PAGE_COLOR==="Green" || process.env.PAGE_COLOR==="green")
@@ -28,30 +30,25 @@ app.get('/color', (req, res) => {
   if (process.env.PAGE_COLOR==="Black" || process.env.PAGE_COLOR==="black")
     return res.send("#262626")
   return res.send("#262626")
+  span.finish();
 })
 app.get('/version', (req, res) => {
+  // Start a new trace
+  const span = tracer.startSpan('/version');
+  // Your logic here
   return res.send(process.version)
+  span.finish();
 })
 app.get('/ipaddress', (req, res) => {
+  // Start a new trace
+  const span = tracer.startSpan('/ipaddress');
+  // Your logic here
   return res.send(ip.address())
+  span.finish();
 })
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-// Setup server to Prometheus scrapes:
-
-app.get('/metrics', async (req, res) => {
-	try {
-		res.set('Content-Type', register.contentType);
-		res.end(await register.metrics());
-	} catch (ex) {
-		res.status(500).end(ex);
-	}
-});
-const port = process.env.PORT || 8080;
-console.log(
-	 	`Server listening to ${port}, metrics exposed on /metrics endpoint`,
-);
 app.listen(8080)
